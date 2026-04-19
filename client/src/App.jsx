43c3320448a78bf5efe21022494c85db6c95d465
Abdul Haq <abdulhaq.se77@gmail.com>
2026-04-19 16:18:44 +0500

    feat: initial monorepo structure for multi-vendor app

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <>
+      <h1 className="text-amber-400">Welcome to the React App!</h1>
+      <p>This is a simple React application.</p>
+    </>
+  );
+}
